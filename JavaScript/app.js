7c2a1effec68135401bced3894041bbc4c852768
Ujwalareddy14 <ujwalav14@gmail.com>
2024-07-01 11:40:40 +0530

    function in javascript

---
+++ JavaScript/app.js
@@ -0,0 +1,27 @@
+let a=20;
+let b= 40;
+console.log(typeof 2+2)
+console.log(1)
+
+function greet()
+{
+    console.log("good morning")
+}
+
+greet()
+
+function add()
+{
+    let a=1;
+    let b=2;
+    console.log(a+b)
+}
+add()
+
+function add1(a , b)
+{
+console.log(a + b)
+}
+
+add1(4,6)
+add1(20,100)
